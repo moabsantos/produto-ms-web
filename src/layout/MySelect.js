cd56f746e4fe23d930ef1142598ced5bc3700e57
Moab Santos <moabsantos.msoftware@gmail.com>
2023-02-24 01:27:06 -0300

    feat: useEffect

--- src/layout/MySelect.js
+++ src/layout/MySelect.js
@@ -4,17 +4,12 @@ import getApi from '../_shared/req-get-http';
 
 const MySelect = (props) => {
 
-    let [options, setOptions] = useState(null);
+    let [options, setOptions] = useState();
     const [valueDefault, setValueDefault] = useState(props.value);
 
     useEffect(() => {
       getApi({ url: `${process.env.REACT_APP_HOST_API}/${props.dominio}` }).then((data) => {
-
-        const items = data.map(item => (
-          <option value={item.id} key={item.id}>{item.name}</option>
-        ))
-
-        setOptions((<>{items}</>))
+        setOptions(data)
       })
     }, [props.dominio])
 
@@ -24,16 +19,19 @@ const MySelect = (props) => {
           id={props.name} 
           onChange={(e) => setValueDefault(e.target.value)}
           value={valueDefault} >
-
-          {options && (options)}
+          {options && options.map(
+            item => (
+              <option value={item.id} key={item.id}>{item.name}</option>
+            )
+          )}
         </select>
     
-    return props.caption ? (<Form.Group as={Row} className="mb-3" controlId={props.name}>
+    return options && props.caption ? (<Form.Group as={Row} className="mb-3" controlId={props.name}>
       <Form.Label column sm={2}>
       {props.caption}
       </Form.Label>
       <Col sm={10}>
-      {options && (mySelect)}
+      {(mySelect)}
       </Col>
     </Form.Group>) 
     : options && (mySelect)
